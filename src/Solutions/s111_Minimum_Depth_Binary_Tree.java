b83b2e7f548ee0b6f78e86c88c37294e7d4dedf9
aditya lochan <adipase82@gmail.com>
2020-10-18 18:59:24 -0400

    solution

--- src/Solutions/s111_Minimum_Depth_Binary_Tree.java
+++ src/Solutions/s111_Minimum_Depth_Binary_Tree.java
@@ -4,13 +4,18 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class s111_Minimum_Depth_Binary_Tree {
-//    Definition for a binary tree node.
     public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
         TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
@@ -18,33 +23,54 @@ public class s111_Minimum_Depth_Binary_Tree {
         }
     }
 
+    //can be solved in 3 ways
+    // recursive call
+    // DFS : LevelOrderTraversal
+    // BFS
     public int minDepth(TreeNode root) {
-            int counter=0;
+//The below one is recursive call solution
+            int min_depth_val = Integer.MAX_VALUE;
             if(root==null){
                 return 0;
             }
-            Queue<TreeNode> queue = new LinkedList();
-            queue.add(root);
-            while(!queue.isEmpty()){
-                TreeNode tempNode= queue.poll();
-                if(tempNode.left==null && tempNode.right==null){
-                    return counter;
-                }else {
-                    if(tempNode.left!=null){
-                        queue.add(tempNode.left);
-                    }
-                    if(tempNode.right!=null){
-                        queue.add(tempNode.right);
-                    }
-                    counter++;
-                }
+            if(root.left==null && root.right==null){
+                return 1;
             }
-            return counter;
+            if(root.left!=null){
+                min_depth_val=Math.min(minDepth(root.left),min_depth_val);
+            }
+            if(root.right!=null){
+                min_depth_val=Math.min(minDepth(root.right),min_depth_val);
+            }
+            return min_depth_val+1;
         }
-        
+//        The below is DFS which is not working. There is additions code in while loop that unable to understand. Check Leetcode solution
+/*        int depth = 1;
+        TreeNode tempNode = new TreeNode(0);
+        Queue<TreeNode> queue = new LinkedList();
+        if (root == null) {
+            return 0;
+        }
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            depth++;
+            tempNode = queue.poll();
+            if (tempNode.left == null && tempNode.right == null) {
+                return depth;
+            }
+            if (tempNode.left != null) {
+                queue.add(tempNode.left);
+            }
+            if (tempNode.right != null) {
+                queue.add(tempNode.right);
+            }
+        }
+        return depth;
+    }*/
+
     public static void main(String[] args) {
-        TreeNode t = new TreeNode(3,new TreeNode(9,null,null),new TreeNode(20,new TreeNode(15,null,null),new TreeNode(7,null,null)));
+        TreeNode t = new TreeNode(3, new TreeNode(9, null, null), new TreeNode(20, new TreeNode(15, null, null), new TreeNode(7, null, null)));
         s111_Minimum_Depth_Binary_Tree s = new s111_Minimum_Depth_Binary_Tree();
-        s.minDepth(t);
+        System.out.println(s.minDepth(t));
     }
 }
