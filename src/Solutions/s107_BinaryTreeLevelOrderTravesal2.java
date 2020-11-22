0d8875209c4e9fcf699739d9f187fadd7e203862
aditya lochan <adipase82@gmail.com>
2020-11-22 16:27:17 -0500

    Solution 107 update

---
+++ src/Solutions/s107_BinaryTreeLevelOrderTravesal2.java
@@ -0,0 +1,43 @@
+package Solutions;
+
+import java.util.List;
+
+public class s107_BinaryTreeLevelOrderTravesal2 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    TreeNode root;
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        s107_BinaryTreeLevelOrderTravesal2 tree_level = new s107_BinaryTreeLevelOrderTravesal2();
+        tree_level.root = new TreeNode(1);
+        tree_level.root.left = new TreeNode(2);
+        tree_level.root.right = new TreeNode(3);
+        tree_level.root.left.left = new TreeNode(4);
+        tree_level.root.left.right = new TreeNode(5);
+
+//        tree_level.levelOrder(tree_level.root);
+    }
+}
